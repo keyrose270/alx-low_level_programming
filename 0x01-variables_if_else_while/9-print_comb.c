adf98315176944e1a8da3204b28e77ead4350bc7
keyrose270 <keyrose302@gmail.com>
2022-12-22 05:27:46 -0800

    tasks 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-int a;
-for (a = 0; a <= 10; a++)
+int num;
+for (num = 0; num <= 9; num++)
 {
-putchar(a + '0');
-if (a < 9);
+putchar((num % 10) + '0');
+if (num == 9)
 continue;
 putchar(',');
 putchar(' ');
