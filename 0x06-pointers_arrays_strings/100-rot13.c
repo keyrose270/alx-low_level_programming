831822af53e34c1df288cfeeeb2ef862200655c7
keyrose270 <keyrose302@gmail.com>
2022-12-25 15:32:30 -0800

    tasks 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,23 +1,41 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * rot13 - function that encodes a string using rot13.
- * @s: string to encode
- * Return: Sucess tasks 8
- */
-char *rot13(char *s)
+* rot13 -  encodes a string using rot13
+* @str:the string targeted
+*Return: Sucess 0 tasks8
+*/
+char *rot13(char *str)
 {
-int h = 0;
-while (s[h] != '\0')
+int index1, index2;
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R',
+'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z', 'a', 'b', 'c', 'd',
+'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p',
+'q', 'r', 's', 't', 'u', 'v',
+'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+'T', 'U', 'V', 'W', 'X', 'Y',
+'Z', 'A', 'B', 'C', 'D', 'E',
+'F', 'G', 'H', 'I', 'J', 'K',
+'L', 'M', 'n', 'o', 'p', 'q',
+'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z', 'a', 'b', 'c',
+'d', 'e', 'f', 'g', 'h', 'i',
+'j', 'k', 'l', 'm'};
+while (str[++index1])
 {
-while (((s[h] >= 'a' && s[h] <= 'm') || (s[h] >= 'A' && s[h] <= 'M')))
+for (index2 = 0; index2 < 52; index2++)
 {
-s[h] += 13;
-h++;
+if (str[index1] == alphabet[index2])
+{
+str[index1] = rot13key[index2];
+break;
 }
-if ((s[h] >=  'n' && s[h] <= 'z') || (s[h] >=  'N' && s[h] <= 'Z'))
-s[h] -= 13;
-h++;
 }
-return (0);
+}
+return (str);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,39 @@
 #include "main.h"
 /**
- * cap_string - converts string words to be capitalized
- * @s: input string to have all words capitalized
- * Return: string after conversion to Capitalize words
- */
-
+* rot13 -  encodes a string using rot13
+* @str:the string targeted
+*Return: Sucess 0 tasks 6
+*/
+char *rot13(char *str)
+{
+int index1, index2;
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R',
+'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z', 'a', 'b', 'c', 'd',
+'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p',
+'q', 'r', 's', 't', 'u', 'v',
+'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+'T', 'U', 'V', 'W', 'X', 'Y',
+'Z', 'A', 'B', 'C', 'D', 'E',
+'F', 'G', 'H', 'I', 'J', 'K',
+'L', 'M', 'n', 'o', 'p', 'q',
+'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z', 'a', 'b', 'c',
+'d', 'e', 'f', 'g', 'h', 'i',
+'j', 'k', 'l', 'm'};
+while (str[++index1]){
+for (index2 = 0; index2 < 52; index2++)
+{
+if (str[index1] == alphabet[index2])
+{
+str[index1] = rot13key[index2];
+break;
+}
+}
+}
+return (str);
+}
