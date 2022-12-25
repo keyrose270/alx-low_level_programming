099531bc420423e96a2b75cc5c1b3bdb52e5a71c
keyrose270 <keyrose302@gmail.com>
2022-12-25 06:56:42 -0800

    tasks 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - checks if 2 strings are of equal value
+ * @s1: destination string.
+ * @s2: the string to compare.
+ * Return: Sucess compare.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int a;
+a = 0;
+while (s1[a] != '\0' && s2[a] != '\0')
+{
+if (s1[a] != s2[a])
+return (s1[a] - s2[a]);	
+a++;
+}
+return (s1[a] - s2[a]);
+}
+
