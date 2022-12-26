c9e640ad88c8de10c127569f2eb647adc9019db8
keyrose270 <keyrose302@gmail.com>
2022-12-25 16:24:37 -0800

    tasks 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_number - prints an integer
+*@i:integer to be printed
+*/
+void print_number(int i)
+{
+unsigned int num = i;
+if (i < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+_putchar((num % 10) + '0');
+}
