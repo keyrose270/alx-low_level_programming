03a654adc6c0719fbecfc23d261999d8a7e71889
keyrose270 <keyrose302@gmail.com>
2022-12-27 04:58:44 -0800

    tasks 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - add multiples of 3 & 5 below 1024
+ * Return: Sucess tasks 13 Always 0.
+ */
+int main(void)
+{
+int a = 0, b = 0;
+while (i < 1024)
+{
+if (a % 3 == 0 || a % 5 == 0)
+b += a;
+a++;
+}
+printf("%d\n", b);
+return (0);
+}
