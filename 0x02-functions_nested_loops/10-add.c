7a1cdab286ce90c2c22b6e06ebf0f0250b743d3f
keyrose270 <keyrose302@gmail.com>
2022-12-27 03:54:13 -0800

    tasks 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers together
+ * @i: first value.
+ * @t: second value .
+ * Return: Sucess tasks 10
+ */
+int add(int i, int t)
+{
+return (i + t);
+}
