7daa170a44b3a46f1cf0381c3301cbd8002ba932
keyrose270 <keyrose302@gmail.com>
2022-12-27 03:05:42 -0800

    tasks 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - return the absolute value of a given integer.
+ * @a: value use to find the absolute value.
+ * Return: Sucess taks 6.
+ */
+int _abs(int a)
+{
+if (a < 0)
+return (-a);
+return (a);
+}
