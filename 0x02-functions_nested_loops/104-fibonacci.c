268d1bdd077a77e706b76dc6adeb9d492aabd2b6
keyrose270 <keyrose302@gmail.com>
2022-12-27 07:19:24 -0800

    tasks 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
 /**
+ * main - main bloc
  * print_fib - prints the first 50 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.
- * @len: the length of with the fib sequance should print to.
- * Return: Sucess tasks 15
+ * starting with 1 and 2, followed by a new line
+ * Return: Sucess tasks 16
  */
-void print_fib(int len)
+int main(void)
 {
-unsigned int target, i;
-unsigned int former_l, former_r;
-unsigned int latter_l, latter_r;
-unsigned int fib_l, fib_r;
-f = 1;
-l = 2;
-target = len - 2;
-for (i = 0 ; i < target ; i++)
+int i;
+unsigned long u1 = 0, u2 = 1, u3;
+unsigned long u1_n1, u1_n2, u2_n1, u2_n2;
+unsigned long n1, n2;
+for (i = 0; i < 92; i++)
 {
-fib = f + l;
-printf("%d : ", i + 4);
-printf("%.0Lf", fib);
-f = l;
-l = fib;
-if (i < target - 1)
+u3 = u1 + u2;
+printf("%lu, ", u3);
+u1 = u2;
+u2 = u3;
+}
+u1_n1 = u1 / 10000000000;
+u2_n1 = u2 / 10000000000;
+u1_n2 = u1 % 10000000000;
+u2_n2 = u2 % 10000000000;
+for (i = 93; i < 99; i++)
+{
+n1 = u1_n1 + u2_n1;
+u2 = u1_n2 + u2_n2;
+if ((u1_n2 + u2_n2) > 9999999999)
+{
+n1 += 1;
+n2 %= 10000000000;
+}
+printf("%lu%lu", n1, n2);
+if (inc != 98)
+printf(", ");
+u1_n1 = u2_n1;
+u1_n2 = u2_n2;
+u2_n1 = n1;
+u2_n2 = n2;
+}
 printf("\n");
-}
-putchar('\n');
-}
 return (0);
 }
