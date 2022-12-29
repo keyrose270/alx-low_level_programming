65c6d779af5615039dc427fd6130a2ffb711b270
keyrose270 <keyrose302@gmail.com>
2022-12-29 07:48:11 -0800

    tasks 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints an aray.
+ * @a: array.
+ * @n: elements to print.
+ */
+void print_array(int *a, int n)
+{
+int b;
+for (b = 0 ; b < n ; b++)
+{
+printf("%d", *(a + b));
+if (b < n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
