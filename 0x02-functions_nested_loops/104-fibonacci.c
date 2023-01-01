276f045e77e050280b0c4f178f1983b6e2ab7016
keyrose270 <keyrose302@gmail.com>
2023-01-01 03:07:57 -0800

    tasks 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 /**
- * main - finds and prints the first 98 Fibonacci numbers,
- * starting with 1 and 2
- * followed by a new line
- * Return: ALways 0 (Success)
- */
+*main - prints out first 98
+*fibonacci suit numbers
+*Return: return 0
+*/
 int main(void)
 {
-unsigned long int a, b, c, b1, b2, C1, C2;
-b = 1;
-c = 2;
-printf("%lu", b);
-for (a = 1; a < 91; a++)
+int inc;
+unsigned long n1 = 0, n2 = 1, n3;
+unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+unsigned long h1, h2;
+for (inc = 0; inc < 92; inc++)
 {
-printf(", %lu", c);
-c = c + b;
-b = c - b;
+n3 = n1 + n2;
+printf("%lu, ", n3);
+n1 = n2;
+n2 = n3;
 }
-b1 = b / 1000000000;
-b2 = b % 1000000000;
-c1 = c / 1000000000;
-c2 = c % 1000000000;
-for (a = 92; a < 99; ++a)
+n1_h1 = n1 / 10000000000;
+n2_h1 = n2 / 10000000000;
+n1_h2 = n1 % 10000000000;
+n2_h2 = n2 % 10000000000;
+for (inc = 93; inc < 99; inc++)
 {
-printf(", %lu", c1 + (c2 / 1000000000));
-printf("%lu", c2 % 1000000000);
-c1 = c1 + b1;
-b1 = c1 - b1;
-c2 = c2 + b2;
-b2 = c2 - b2;
+h1 = n1_h1 + n2_h1;
+h2 = n1_h2 + n2_h2;
+if ((n1_h2 + n2_h2) > 9999999999)
+{
+h1 += 1;
+h2 %= 10000000000;
+}
+printf("%lu%lu", h1, h2);
+if (inc != 98)
+printf(", ");
+n1_h1 = n2_h1;
+n1_h2 = n2_h2;
+n2_h1 = h1;
+n2_h2 = h2;
 }
 printf("\n");
 return (0);
