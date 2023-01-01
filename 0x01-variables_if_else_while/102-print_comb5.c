d7d9b2631546c4f573628775f01eeb0355bb0b23
keyrose270 <keyrose302@gmail.com>
2023-01-01 02:51:16 -0800

    tasks 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,24 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Succes)
+ * main - Printing combinations of two two-digit numbers
+ * Return: Always 0.
  */
 int main(void)
 {
-int tho;
-int hun;
-int ten;
-int bas;
-for ( tho = 0 ; tho < 10 ; tho++)
-{	
-for (hun = 0 ; hun <= 10 ; hun++)
-{	
-for (ten = 0 ; ten < 10 ; ten++)
+int nb1, nb2;
+for (nb1 = 0; nb1 <= 98; nb1++)
 {
-for ( bas = 0 ; bas < 10 ; bas++)
+for (nb2 = nb1 + 1; nb2 <= 99; nb2++)
 {
-putchar('0' + tho);
-putchar('0' + hun);
-putchar(32);
-putchar('0' + ten);
-putchar('0' + bas);
-if (!(tho == 9 && hun == 8))
-{
+putchar((nb1 / 10) + '0');
+putchar((nb1 % 10) + '0');
+putchar(' ');
+putchar((nb2 / 10) + '0');
+putchar((nb2 % 10) + '0');
+if (nb1 == 98 && nb2 == 99)
+break;
 putchar(',');
-putchar(32);
-}
-bas++;
-}
-}
+putchar(' ');
 }
 }
 putchar('\n');
