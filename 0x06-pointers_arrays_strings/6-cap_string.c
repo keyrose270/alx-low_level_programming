f11517addd970e8007aaa492b6971c3a85e528cd
keyrose270 <keyrose302@gmail.com>
2023-01-01 03:33:13 -0800

    tasks 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,23 @@
 #include "main.h"
 /**
-* rot13 -  encodes a string using rot13
-* @str:the string targeted
-*Return: Sucess 0 tasks 6
-*/
-char *rot13(char *str)
+ * *cap_string - function
+ * @str: Pointer
+ * Return: poniter return to function.
+ */
+char *cap_string(char *str)
 {
-int index1, index2;
-char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-'G', 'H', 'I', 'J', 'K', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',
-'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z', 'a', 'b', 'c', 'd',
-'e', 'f', 'g', 'h', 'i', 'j',
-'k', 'l', 'm', 'n', 'o', 'p',
-'q', 'r', 's', 't', 'u', 'v',
-'w', 'x', 'y', 'z'};
-char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-'T', 'U', 'V', 'W', 'X', 'Y',
-'Z', 'A', 'B', 'C', 'D', 'E',
-'F', 'G', 'H', 'I', 'J', 'K',
-'L', 'M', 'n', 'o', 'p', 'q',
-'r', 's', 't', 'u', 'v', 'w',
-'x', 'y', 'z', 'a', 'b', 'c',
-'d', 'e', 'f', 'g', 'h', 'i',
-'j', 'k', 'l', 'm'};
-while (str[++index1]){
-for (index2 = 0; index2 < 52; index2++)
+int symb[14] = {' ', '\t', '\n', ',', ';', '.', '!',
+'?', '"', '(', ')', '{', '}'};
+int i, j;
+for (i = 0; str[i] != '\0'; i++)
 {
-if (str[index1] == alphabet[index2])
+if (str[0] >= 97 && str[0] <= 122)
 {
-str[index1] = rot13key[index2];
-break;
+str[0] = str[0] - 32;
 }
-}
+for (j = 0; j < 14; j++)
+if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == symb[j])
+str[i] = str[i] - 32;
 }
 return (str);
 }
