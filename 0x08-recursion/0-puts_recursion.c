3066adc90583543b900dec788e8aeef8f19aea3d
keyrose270 <keyrose302@gmail.com>
2023-01-04 08:47:58 -0800

    tasks 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string .
+ * @s: string .
+ * Return: Success tasks 0
+ */
+void _puts_recursion(char *s)
+{
+if (!*s)
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion((s + 1));
+}
